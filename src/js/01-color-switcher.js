73dbfffd6d5cc1dbcb1e3e625423ae4ff0032a6c
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-09-05 23:56:11 +0200

    fixed css

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,9 @@ const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 
+
+btnStart.className = 'button-start-centered';
+btnStop.className = 'button-stop-centered';
 let timeId = null;
 
 btnStart.addEventListener('click', () => {
